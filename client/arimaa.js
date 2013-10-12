a5789d09102f7215d40e88510f25a2c20c3a7066
Andrew Hull <aghull@gmail.com>
2013-10-12 16:32:12 -0400

    images

--- client/arimaa.js
+++ client/arimaa.js
@@ -1,10 +1,5 @@
 $(function() {
   client = new GameClient(arimaa, $('#board'), $('#questions'));
-  client.display = {
-    piece: function($) {
-      return '♟♞♝♜♛♚♙♘♗♖♕♔'[parseInt($.attr('strength'), 10) + ($.attr('player')==1 ? 5 : -1)];
-    }
-  };
 
   client.game.start();
 });
